d161be6b126d410cace26930967bd82b0c658ea8
HugoLima <hugo8romao@gmail.com>
2020-10-13 15:15:59 -0400

    Detalhe do orfanato

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,6 +6,7 @@ const routes = Router();
 // MVC - Model, Views, Controllers
 
 routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
